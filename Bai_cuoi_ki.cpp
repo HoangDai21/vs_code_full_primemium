933f41e6382bad4fccab1aaf6dcedeb494fee97e
HoangDai21 <166018254+HoangDai21@users.noreply.github.com>
2024-05-22 16:46:11 +0700

    add

--- Bai_cuoi_ki.cpp
+++ Bai_cuoi_ki.cpp
@@ -1,1 +1,16 @@
+#include<iostream>
+uisng namespace std;
 
+struct Ho_Dan {
+    char Ma_Ho[5];
+    char Ten_Chu_Ho[25];
+    int So_Thanh_Vien;
+    float Muc_Thu_Nhap;
+};
+
+void NhapTT(Ho_Dan*Ho_Dan, int n){
+    for(int i = 0; i < n; i++){
+        cout << "Nhap Ma Ho: ";
+        cin.getline(Ho_Dan->Ho_Dan,5);
+    }
+}
